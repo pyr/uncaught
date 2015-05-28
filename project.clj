583bf0eeb33732ecd3246d86d87221a931638b16
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-05-28 10:43:22 +0200

    project: sanitize

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.spootnik/uncaught "0.5.0"
-  :description "catch uncaught exceptions with ease"
+(defproject org.spootnik/uncaught "0.5.1"
+  :description "handle uncaught exceptions with ease"
   :url "https://github.com/pyr/uncaught"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "https://github.com/pyr/uncaught/tree/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.7.0-RC1"]])
