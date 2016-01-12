966d437e26f213dea1edf21417db1fe1598c32b7
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-01-12 22:42:58 +0100

    tests: add reset handler test

--- test/spootnik/uncaught_test.clj
+++ test/spootnik/uncaught_test.clj
@@ -34,3 +34,20 @@
       (Thread/sleep 200)
       @p
       (is (= 3 (count @errors))))))
+
+(deftest unset-handler-test
+  (let [queue  (ArrayBlockingQueue. 4)
+        tpe    (ThreadPoolExecutor. 4 4 1 TimeUnit/MINUTES queue)
+        p      (promise)
+        errors (atom nil)]
+    (testing "Replace uncaught handler"
+      (with-uncaught e (swap! errors conj e))
+      (.execute tpe (Thread. (fn [] (- nil 0))))
+      (Thread/sleep 200)
+      (with-uncaught e)
+      (.execute tpe (Thread. (fn [] (- nil 0))))
+      (.execute tpe (Thread. (fn [] (- nil 0))))
+      (.execute tpe (Thread. (fn [] (deliver p nil))))
+      (Thread/sleep 200)
+      @p
+      (is (= 1 (count @errors))))))
