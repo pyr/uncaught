1065ace4141f7b811eb640e57a470ca72de53316
Pierre-Yves Ritschard <pyr@spootnik.org>
2017-12-22 14:21:30 +0100

    project: catch reflection warnings

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/pyr/uncaught"
   :license {:name "MIT License"
             :url "https://github.com/pyr/uncaught/tree/master/LICENSE"}
+  :profiles {:dev {:global-vars {*warn-on-reflection* true}}}
   :dependencies [[org.clojure/clojure "1.9.0"]])
