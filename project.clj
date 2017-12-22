ff21e62fa5b1f607fa51589072f418ab6aedb6a5
Pierre-Yves Ritschard <pyr@spootnik.org>
2017-12-22 13:34:52 +0100

    project: release 0.5.4

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject spootnik/uncaught "0.5.3"
+(defproject spootnik/uncaught "0.5.4"
   :description "Handle uncaught exceptions with ease"
   :url "https://github.com/pyr/uncaught"
   :license {:name "MIT License"
             :url "https://github.com/pyr/uncaught/tree/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]])
